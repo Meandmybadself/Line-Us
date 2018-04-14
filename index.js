260e12e184b9dd2e4bb23427e380857ef8d32f25
Jeffery Bennett <jeffery@permanentrecord.io>
2018-04-13 22:26:23 -0500

    Connecting / converting svg.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const dns = require('dns')
 const Telnet = require('telnet-client')
 const fs = require('fs')
-const SVGO = require('svgo')
 const { exec } = require('child_process')
 
 const connection = new Telnet()
@@ -13,59 +12,64 @@ if (process.argv.length < 3) {
 
 const svgPath = process.argv[2]
 
+const cleanSVG = svgPath =>
+  new Promise((resolve, reject) => {
+    exec(`./node_modules/.bin/svgo --output=- --pretty ${svgPath}`, (err, stdout, stderr) => {
+      if (err || stderr) {
+        return reject(err || stderr)
+      }
+
+      return resolve(stdout)
+    })
+  }
+  )
+
+const convertSVG = svg =>
+  new Promise((resolve, reject) => {
+    exec(`echo '${svg}' | node ./lib/svg2gcode --tooldiameter 1`, (err, stdout, stderr) => {
+      if (err || stderr) {
+        return reject(err || stderr)
+      }
+      return resolve(stdout)
+    })
+  })
+
 if (!fs.existsSync(svgPath)) {
   console.log(`Could not locate an SVG at ${svgPath}`)
   process.exit(1)
 } else {
-  //let svg = fs.readFileSync(svgPath, {encoding: 'utf8'})
   // Optimize / clean the SVG.
-  exec(`./node_modules/.bin/svgo --output=-  --pretty ${svgPath}`, (err, stdout, strerr) => {
-    if (err || stderr) {
-      console.log("Error while simplifying svg", err, stderr)
-      process.exit(1)
-    } 
-    console.log('stdout', stdout)
-  })
+  cleanSVG(svgPath)
+    .then(cleanedSVG => {
+      convertSVG(cleanedSVG.trim())
+        .then(gcode => {
+          // The Line-Us runs at 'line-us.local'.
+          // Find the IP address (because the telnet client can't resolve string machine names)
+          dns.lookup('line-us.local', (err, addresses) => {
+            if (err) {
+              console.log(err)
+              process.exit()
+            }
 
-  
-  // s.optimize(svg, {path: svgPath})
-  //   .then(result => {
-  //     console.log('result', result)
-  //   })
-  //   .catch(e => {
-  //     console.log('e', e)
-  //   })
-  // console.log(s2g.runWithSVG(svg))
+            // If we've resolved the IP, connect.
+            if (addresses && addresses.length) {
+              console.log('Connecting to ', addresses)
+              connect(addresses)
+                .then(prompt => {
+                  console.log('Connected.', prompt)
+                  prompt.exec(gcode)
+                })
+            } else {
+              console.log('Could not resolve hostname.')
+            }
+          })
+
+          const connect = host => connection.connect({
+            host,
+            port: 1337,
+            shellPrompt: /hello .+/
+          })
+        })
+        .catch(console.log)
+    })
 }
-
-// process.exit()
-
-
-
-// // The Line-Us runs at 'line-us.local'.
-// // Find the IP address (because the telnet client can't resolve string machine names)
-// dns.lookup('line-us.local', (err, addresses) => {
-//   if (err) {
-//     console.log(err)
-//     process.exit()
-//   }
-
-//   // If we've resolved the
-//   if (addresses && addresses.length) {
-//     console.log('Connecting to ', addresses)
-//     connect(addresses)
-//       .then(prompt => {
-//         console.log('Connected.', prompt)
-//         // Convert SVG into GCode.
-//         console.log(s2g.runWithSVG(svg))
-//       })
-//   } else {
-//     console.log('Could not resolve local hostname.')
-//   }
-// })
-
-// const connect = host => connection.connect({
-//   host,
-//   port: 1337,
-//   shellPrompt: /hello .+/
-// })
